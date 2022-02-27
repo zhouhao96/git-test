8226608694cae70e053da69166c1e84f8dff3881
zhouhao <1121198486@qq.com>
2022-02-27 14:06:28 +0800

    first commit

---
+++ src/main/java/com/zh/GitTest.java
@@ -0,0 +1,11 @@
+package com.zh;
+
+/**
+ * @author zhouhao
+ * @create 2022-02-27 13:58
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
